f822aaed6380cfb09e180d76a32777ca41462941
SHAKER <oshaker@beltonefinancial.com>
2020-10-20 12:18:47 +0200

    Fix: Fixed Reset Password Message

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'subject' => 'Hey! Reset Password Notification',
-    'line1' => 'You requested here you go!', 
+    'subject' => 'Reset Password Notification',
+    'line1' => 'You requested to reset your password', 
     'action' => 'Reset Password',
     'line2' => 'This password reset link will expire in :count minutes.',
     'line3' => 'If you did not request a password reset, no further action is required. Token: ==>'
